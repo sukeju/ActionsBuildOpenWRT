ef652269daf048e83d300ac13708477d5dae8000
sukeju <sukeju@gmail.com>
2025-12-26 20:41:28 +0800

    Add OpenClash source to feeds.conf.default

--- diy-part1.sh
+++ diy-part1.sh
@@ -8,6 +8,7 @@
 #sed -i '$a src-git passwall https://github.com/xiaorouji/openwrt-passwall' ./feeds.conf.default
 sed -i '$a src-git helloworld https://github.com/fw876/helloworld.git' ./feeds.conf.default
 #sed -i '$a src-git nikki https://github.com/nikkinikki-org/OpenWrt-nikki.git;main' ./feeds.conf.default
+sed -i '$a src-git openclash https://github.com/vernesong/OpenClash.git' ./feeds.conf.default
 
 # homeproxy
 #git clone --depth=1 https://github.com/immortalwrt/homeproxy.git package/homeproxy
